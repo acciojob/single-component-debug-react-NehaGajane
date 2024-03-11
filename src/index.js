bb942fd3606a4f356268c2044604c3e1e1090f55
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-03-11 15:41:14 +0530

    Modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("main"));
-root.render(
-    <App/>
+const root = document.getElementById("root");
+ReactDOM.render(
+    <App/>, root
 )
